2e8e2532db54655bf069a48dd79eae29aa8d2109
ShivamSinghCodes <[shivamsinghde@gmail.com]>
2023-02-18 14:53:37 +0530

    Upto Question 9 solved

---
+++ 9_True_False/truefalse.js
@@ -0,0 +1,26 @@
+// Write three JavaScript statement example which provide truthy value.
+// Write three JavaScript statement example which provide falsy value.
+
+
+// Truthy value
+let a = 4 > 2;
+let b = "string" > "String";
+let c = 9 != 3;
+let f = false || true;
+let g = 8 == '8';
+
+
+// Falsy value
+let d = 8 === "8";
+let e = true && false;
+let h = 8 >= 88;
+
+
+console.log("Expression is: " ,a);
+console.log("Expression is: " ,b);
+console.log("Expression is: ",c);
+console.log("Expression is: ",f);
+console.log("Expression is: ",g);
+console.log("Expression is: ",d);
+console.log("Expression is: ",e);
+console.log("Expression is: ",h);
